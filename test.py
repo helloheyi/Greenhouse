e0a3842c9ca34cad9159b51215e2ef0912bf8fe1
helloheyi <yhe38095@gmail.com>
2018-01-12 12:08:42 -0500

    third change
    This is my third change

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-#!wing
+  #!wing
 #!version=6.0
 ##################################################################
 # Wing IDE project file                                          #
@@ -18,3 +18,41 @@ Redshift:
     port : "5439"
     db : "production"
 
+
+
+
+####third change
+/**
+    * Create a Point class to represent any point in a Cartesian coordinate system.
+        * The following instance methods must be supported:
+            *
+                * @author Yi.He
+                    *
+                        */
+
+// make variables
+public class Point {
+    private int ID;
+    private double x;
+    private double y;
+    private int countPoint = 0;
+    private static int countActiveNumber = 0;
+    
+    /**
+        * 1.constructor: one that allows you to specify the x and y coordinates of
+            * the point, and one that initializes the coordinates to 0.0.
+                *
+                    * @param x
+                        *            the value of x
+                            * @param y
+                                *            the value of y
+                                    */
+
+// two constructors
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+        countPoint++;
+        this.ID = countPoint;
+        countActiveNumber++;
+}
